586b751cdd381ff3ca8702e147958177b753ed4f
mustifk <furkan_kaz@hotmail.com>
2020-12-24 00:27:56 +0300

    main index

--- OnlineWarehousingInformationSystem/OnlineWarehousingInformationSystem/Controllers/MainController.cs
+++ OnlineWarehousingInformationSystem/OnlineWarehousingInformationSystem/Controllers/MainController.cs
@@ -11,15 +11,28 @@ namespace OnlineWarehousingInformationSystem.Controllers
     public class MainController : Controller
     {
         OWISDBEntities db = new OWISDBEntities();
-        // GET: Main
+
+
         public ActionResult Index()
         {
-            var userId = (int)Session["UserId"];
-            return View(db.Users.Where(s => s.userID == userId).ToList());
+            var rec = new List<Tuple<int,string,string>>
+            {
+                new Tuple<int,string,string>(db.Users.Count(),"User" , "Users"),
+                new Tuple<int,string,string>(db.Warehouses.Count(),"Warehouse" , "Warehouses"),
+                new Tuple<int,string,string>(db.Products.Count(),"Product" , "Products"),
+                new Tuple<int,string,string>(db.Suppliers.Count(),"Supplier" , "Suppliers"),
+                new Tuple<int,string,string>(db.Packages.Count(),"Package" , "Packages"),
+                new Tuple<int,string,string>(db.Shipping.Count(),"Shipping" , "Shippings"),
+            };
+
+            ViewData["rec"] = rec;
+
+            return View();
         }
         public ActionResult Contact()
         {
             return View();
         }
+
     }
 }
