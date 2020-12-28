4f64cffec46d3f376d08f861fb50e06d398d4dd4
Okan <67058617+Vasseurr@users.noreply.github.com>
2020-12-28 19:37:08 +0300

    packageall

--- OnlineWarehousingInformationSystem/OnlineWarehousingInformationSystem/Controllers/PackagesController.cs
+++ OnlineWarehousingInformationSystem/OnlineWarehousingInformationSystem/Controllers/PackagesController.cs
@@ -14,7 +14,51 @@ namespace OnlineWarehousingInformationSystem.Controllers
         public ActionResult Index()
         {
             var shipping = db.Packages.Where(o => o.packageID > 0).Select(o => o);
+            
             return View(shipping);
+        }
+
+        public ActionResult AddPackage()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddPackage(Packages package)
+        {
+            db.Packages.Add(package);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult DetailPackage(int id)
+        {
+            var query = db.PackageContents.Where(o => o.packageID == id).Select(o => o);
+            return View(query);
+        }
+
+        public ActionResult EditPackage(int id)
+        {
+            var query = db.Packages.Find(id);
+            return View(query);
+        }
+
+        [HttpPost]
+        public ActionResult EditPackage(Packages package)
+        {
+            Packages u_package = db.Packages.Where(o => o.packageID == package.packageID).FirstOrDefault();
+            u_package.createdTime = package.createdTime;
+            u_package.packageStatus = package.packageStatus;
+            u_package.notes = package.notes;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public RedirectToRouteResult DeleteFromPackages(int id)
+        {
+            db.Packages.RemoveRange(db.Packages.Where(o => o.packageID == id));
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
