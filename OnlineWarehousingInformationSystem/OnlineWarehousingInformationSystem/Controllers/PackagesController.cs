fb8708ea47bb65c32d0eaf99fa5ca9e4380a3550
Okan <67058617+Vasseurr@users.noreply.github.com>
2020-12-28 19:14:44 +0300

    packages

---
+++ OnlineWarehousingInformationSystem/OnlineWarehousingInformationSystem/Controllers/PackagesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using OnlineWarehousingInformationSystem.Models;
+namespace OnlineWarehousingInformationSystem.Controllers
+{
+    public class PackagesController : Controller
+    {
+        // GET: Packages
+        OWISDBEntities db = new OWISDBEntities();
+        public ActionResult Index()
+        {
+            var shipping = db.Packages.Where(o => o.packageID > 0).Select(o => o);
+            return View(shipping);
+        }
+    }
+}
